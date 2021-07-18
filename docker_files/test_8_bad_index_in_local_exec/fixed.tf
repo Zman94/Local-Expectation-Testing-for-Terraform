ab068a45720a776a83a1cb75c1a5f95fa0db9893
Zman94 <zgleason94@gmail.com>
2021-07-18 17:29:47 -0400

    add test 8

---
+++ docker_files/test_8_bad_index_in_local_exec/fixed.tf
@@ -0,0 +1,23 @@
+resource "aws_vpc" "test_8_vpc" {
+  count      = 2
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "test_8"
+  }
+
+}
+
+resource "null_resource" "possible_error" {
+  provisioner "local-exec" {
+    command = "echo ${aws_vpc.test_8_vpc[1].id}"
+  }
+}
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.test_8_vpc[0].id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "my_public_subnet"
+  }
+}
